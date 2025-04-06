a9c580a321e32dfd875a8f615fd916fe5c03b12f
farzan <farzan.md1384@gmail.com>
2025-04-06 16:26:33 +0330

    rotation of cursor and hexagon

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Main {
+import View.MainMenu;
+
+public class Main {
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> new MainMenu());
+    }
 }
+
